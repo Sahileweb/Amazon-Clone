3ba9c489cdb1d0bcd4764648d71115a450ca7017
Sahileweb <sahil2003mundhe@gmail.com>
2025-06-20 14:57:10 +0530

    fix: clean CRA build for Vercel deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';  
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <BrowserRouter>  
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
-
