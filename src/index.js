0c947fdc9ec06311a0af45ddf331a852366c2f8f
Sahileweb <sahil2003mundhe@gmail.com>
2025-06-20 14:20:14 +0530

    fix: use BrowserRouter and add vercel.json for Vercel routing

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-import { HashRouter } from 'react-router-dom';
-
-
+import { BrowserRouter } from 'react-router-dom';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-   
-      
-        
-          <HashRouter>
-            <App />
-          </HashRouter>
-        
-      
-    
+    <BrowserRouter>  
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
+
